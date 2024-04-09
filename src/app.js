bf6e92f885b4624a1cd6f692193367964d377e17
GwijaCodes <133336462+GwijaCodes@users.noreply.github.com>
2024-04-09 11:31:49 +0200

    Add files via upload

--- src/app.js
+++ src/app.js
@@ -1,39 +1,46 @@
-//dati array
-const settimana = ["Domenica", "Lunedì", "Martedì", "Mercoledì", "Giovedì", "Venerdì", "Sabato"];
-const mesi = ["Gennaio", "Febbraio", "Marzo", "Aprile", "Maggio", "Giugno", "Luglio", "Agosto", "Settembre", "Ottobre", "Novembre", "Dicembre"];
-let myDays = document.querySelectorAll('label p span');
-let raccolta = [...myDays].map(el => el.textContent);
-setTimeout(() => { }, 100)
-
-
 //mostra data
 let date = new Date();
 let dayN = date.getDay();
+//mese e settimana in italiano
+const settimana = ["Domenica", "Lunedì", "Martedì", "Mercoledì", "Giovedì", "Venerdì", "Sabato"];
+const mesi = ["Gennaio", "Febbraio", "Marzo", "Aprile", "Maggio", "Giugno", "Luglio", "Agosto", "Settembre", "Ottobre", "Novembre", "Dicembre"];
 let weekday = settimana[dayN];
 let month = mesi[date.getMonth()];
+//data di oggi in italiano
 let day = date.getDate();
 let oggi = document.querySelector('#oggi').innerHTML = `${weekday} ${day} ${month}`
 
 //mostra cosa buttare oggi e domani
-let today = document.querySelector('#today').innerText = `${raccolta[dayN]}`;
+
+let myDays = document.querySelectorAll('label p span');
+let raccolta = [...myDays].map(el => el.textContent);
+let today;
+setInterval(() => {
+    today = document.querySelector('#today').innerHTML = 'Carta';
+    if (dayN >= raccolta.length - 1) {
+        domani.innerHTML = raccolta[0];
+    } else {
+        domani.innerHTML = raccolta[dayN + 1];
+    }
+
+    switch (today) {
+        case 'Plastica':
+            trashbin.style.backgroundPositionX = 700 - (100 * 3) + 'px';
+        case 'Umido':
+            trashbin.style.backgroundPositionX = 700 - (100 * 2) + 'px';
+        case 'Carta':
+            trashbin.style.backgroundPositionX = 700 - (100 * 4) + 'px';
+
+    }
+
+}, 200)
+
 let domani = document.querySelector('#tomorrow');
-    //trashbin
+//trashbin
 const trashbin = document.querySelector('.trashbin');
-trashbin.style.backgroundPositionX = 1365 - (100 * dayN) + 'px';
 
 //loop da sabato a domenica
-if (dayN >= raccolta.length - 1) {
-    domani.innerHTML = raccolta[0];
-} else {
-    domani.innerHTML = raccolta[dayN + 1];
-}
 
-
-
-const editPanel = document.querySelector('.edit-panel');
-const list = document.querySelector('.list');
-const edit = document.getElementsByClassName('edit');
-const dt = document.getElementsByClassName('day-trash');
 
 
 
